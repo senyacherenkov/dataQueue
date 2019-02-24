e7723b8871f4846405cf616931202825ac46748c
senyacherenkov <senyacherenkov@yandex.ru>
2019-02-24 20:02:12 +0300

    almost done

--- dataqueue.h
+++ dataqueue.h
@@ -10,126 +10,163 @@
 
 template <typename T, std::size_t QUEUE_SIZE = 256, class Allocator = std::allocator<T>,
           typename = typename std::enable_if<std::is_nothrow_copy_constructible<T>::value>::type,
-          typename = typename std::enable_if<std::is_default_constructible<T>::value>::type
-          >
+          typename = typename std::enable_if<std::is_default_constructible<T>::value>::type>
 class DataQueue{
+private:
+    struct node
+    {
+        std::shared_ptr<T> data {nullptr};
+        std::unique_ptr<node> next {nullptr};
+        int number = 0;
+    };
+
 public:
-    explicit DataQueue() = default;
+    DataQueue():
+        m_head(std::make_unique<node>()), m_tail(m_head.get())
+    {}
 
     DataQueue(const DataQueue& other) = delete;
-    DataQueue(DataQueue&& other) = delete;
-
-    DataQueue& operator= (const DataQueue& other) = delete;
-    DataQueue& operator= (DataQueue&& other) = delete;
+    DataQueue& operator= (const DataQueue& other) = delete;    
 
     ~DataQueue() {
-        std::unique_lock<std::mutex> guard1(m_headMutex, std::defer_lock);
-        std::unique_lock<std::mutex> guard2(m_tailMutex, std::defer_lock);
-        std::lock(guard1, guard2);
-
-        if(!m_head)
-            return;
-        else {
-            QueueNode* temp = m_head->next;
-            m_allocator.deallocate(m_head, sizeof(QueueNode));
-
-            while(temp) {
-                QueueNode* next = temp->next;
-                m_allocator.deallocate(temp, sizeof(QueueNode));
-                temp = next;
-            }
-        }
     }
 
-    bool try_push(const T& item) {
-        if(!m_isFull)
+    bool tryPush(const T& newItem) {
+        std::shared_ptr<T> newData = std::make_shared<T>(newItem);
+
+        std::unique_ptr<node> newVertex(std::make_unique<node>());
+
         {
-            if(m_isEmpty) {
-                {
-                    std::lock_guard<std::mutex> lckHead{m_headMutex};
-                    m_head = m_allocator.allocate(sizeof(QueueNode));
-                    m_allocator.construct(m_head, item);
+            std::lock_guard<std::mutex> tail_lock(m_tailMutex);
 
+            if(m_tail->number > QUEUE_SIZE)
+                return false;
 
-                    std::lock_guard<std::mutex> lckTail{m_tailMutex};
-                    m_tail = m_head;
-                }
-                m_isEmpty = false;
-            }
-            else {
-                QueueNode* temp = m_allocator.allocate(sizeof(QueueNode));
-                m_allocator.construct(temp, item);
-
-                std::unique_lock<std::mutex> guard1(m_headMutex, std::defer_lock);
-                std::unique_lock<std::mutex> guard2(m_tailMutex, std::defer_lock);
-                std::lock(guard1, guard2);
-
-                if(m_tail == m_head)
-                    m_head->next = temp;
-                else
-                    m_tail->next = temp;
-
-                guard1.unlock();
-                m_tail = temp;
-            }
-
-            ++m_size;
-            if(m_size == QUEUE_SIZE)
-                m_isFull = true;
-
-            return true;
+            m_tail->number++;
+            m_tail->data = newData;
+            node* const newTail = newVertex.get();
+            m_tail->next = std::move(newVertex);
+            m_tail = newTail;
         }
-        return false;
-    }
 
-    void wait_push(const T& item) {
-        while(m_isFull);
-        try_push(item);
-    }
-
-    bool try_pop(T& item) {
-        if(m_isEmpty)
-            return false;
-
-        std::lock_guard<std::mutex> guard1(m_headMutex);
-        item = m_head->m_item;
-        QueueNode* temp = m_head->next;
-
-        m_allocator.destroy(m_head);
-        m_allocator.deallocate(m_head, 1);
-
-        m_head = temp;
-
+        m_dataAwaiting.notify_one();
         return true;
     }
 
-    void wait_pop(T& item) {
-        while(m_isEmpty);
-        try_pop(item);
+    void waitPush(const T& item) {
+
+    }
+
+    bool tryPop(T& item) {
+        std::unique_ptr<node> const oldHead = tryPopHead(item);
+        return oldHead;
+    }
+
+    std::shared_ptr<T> tryPop() {
+        std::unique_ptr<node> const oldHead = tryPopHead();
+        return oldHead? oldHead->data: std::shared_ptr<T>();
+    }
+
+    void waitPop(T& item) {
+        waitPopHead(item);
+    }
+
+    std::shared_ptr<T> waitPop() {
+        std::unique_ptr<node> const oldHead = waitPopHead();
+        return oldHead->data;
     }
 
     void clear();    
 
-    bool empty() const { return m_isEmpty; }
-    bool full() const {return m_isFull; }
-    void stop_waiting();
-private:
-    struct QueueNode {
-        QueueNode() = default;
-        QueueNode(const T& item):
-            m_item(item)
-        {}
+    bool empty() const {
+        std::lock_guard<std::mutex> headLock(m_headMutex);
+        return (m_head.get() == getTail());
+    }
 
-        T m_item;
-        QueueNode* next = nullptr;
-    };
+    bool full() const {}
+    void stop_waiting();
+
 private:
-    Allocator           m_allocator;
-    QueueNode*          m_head = nullptr;
-    QueueNode*          m_tail = nullptr;
-    std::mutex          m_headMutex;
-    std::mutex          m_tailMutex;
-    std::atomic<bool>   m_isEmpty = true;
-    std::atomic<bool>   m_isFull = false;
-    std::atomic<size_t> m_size = 0;
+    node* getTail()
+    {
+        std::lock_guard<std::mutex> tailLock(m_tailMutex);
+        return m_tail;
+    }
+
+    /*****POP AREA*****/
+    std::unique_ptr<node> popHead()
+    {
+        std::unique_ptr<node> oldHead = std::move(m_head);
+        m_head = std::move(oldHead->next);
+        return oldHead;
+    }
+
+    std::unique_ptr<node> tryPopHead()
+    {
+        std::unique_lock<std::mutex> headLock(m_headMutex);
+        if(m_head.get() == getTail())
+        {
+            return std::unique_ptr<node>();
+        }
+        getTail()->number--;
+        return popHead();
+    }
+
+    std::unique_ptr<node> tryPopHead(T& item)
+    {
+        std::unique_lock<std::mutex> headLock(m_headMutex);
+        if(m_head.get() == getTail())
+        {
+            return std::unique_ptr<node>();
+        }
+        getTail()->number--;
+        item = std::move(*m_head->data);
+        return popHead();
+    }
+
+    std::unique_lock<std::mutex> waitForData()
+    {
+        std::unique_lock<std::mutex> headLock(m_headMutex);
+        m_dataAwaiting.wait(headLock, [&](){ return m_head.get() != getTail(); });
+        return headLock;
+    }
+
+    std::unique_ptr<node> waitPopHead()
+    {
+        std::unique_lock<std::mutex> headLock(waitForData());
+        return popHead();
+    }
+
+    std::unique_ptr<node> waitPopHead(T& item)
+    {
+        std::unique_lock<std::mutex> headLock(waitForData());
+        item = std::move(m_head->data);
+        return popHead();
+    }
+
+    /*****POP AREA END*****/
+
+    /*****PUSH AREA*****/
+    std::unique_lock<std::mutex> waitForRoom()
+    {
+        std::unique_lock<std::mutex> tailLock(m_tailMutex);
+        m_roomAwaiting.wait(tailLock, [&](){ return m_tail->number <= QUEUE_SIZE; });
+        return tailLock;
+    }
+
+    void waitPushData(T& item)
+    {
+        std::shared_ptr<T> newData = std::make_shared<T>(item);
+        std::unique_lock<std::mutex> tailLock(waitForRoom());
+        m_tail->data = newData;
+    }
+    /*****PUSH AREA END*****/
+private:
+    Allocator               m_allocator;
+    std::mutex              m_headMutex;
+    std::unique_ptr<node>   m_head;
+    std::mutex              m_tailMutex;
+    node*                   m_tail;
+    std::condition_variable m_dataAwaiting;
+    std::condition_variable m_roomAwaiting;
 };
