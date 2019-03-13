8f6f475075744afff2e32eaf6ff4c5bdae9a10d6
senyacherenkov <senyacherenkov@yandex.ru>
2019-03-13 22:22:49 +0300

    store to file in progress. Again

--- dataqueue.h
+++ dataqueue.h
@@ -7,11 +7,64 @@
 #include <condition_variable>
 #include <type_traits>
 #include <atomic>
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <cstring>
+#include <iostream>
+
+namespace {
+    template<typename T, typename = typename std::enable_if<std::is_pod<T>::value>::type>
+    std::ostream& write(const T& value, std::ofstream& ifs)
+    {        
+        return ifs.write(reinterpret_cast<const char*>(&value), sizeof (T));
+    }
+
+    template<typename T>
+    typename std::enable_if<!std::is_pod<T>::value, std::ostream&>::type
+    write(const T& value, std::ofstream& ifs)
+    {
+        return value.serialize(ifs);
+    }
+
+    template<typename T, typename = typename std::enable_if<std::is_pod<T>::value>::type>
+    T read(std::ifstream& ofs)
+    {
+        T value;
+        ofs.read(reinterpret_cast<char*>(&value), sizeof(T));
+        return value;
+    }
+
+    template<typename T>
+    typename std::enable_if<!std::is_pod<T>::value, T>::type
+    read(std::ifstream& ofs)
+    {
+        T value;
+        return value.deserialize(ofs);
+    }
+
+    decltype(std::chrono::seconds().count()) getSecondsSinceEpoch()
+    {
+        // get the current time
+        const auto now     = std::chrono::system_clock::now();
+
+        // transform the time into a duration since the epoch
+        const auto epoch   = now.time_since_epoch();
+
+        // cast the duration into seconds
+        const auto seconds = std::chrono::duration_cast<std::chrono::seconds>(epoch);
+
+        // return the number of seconds
+        return seconds.count();
+    }
+}
+
+namespace threadsafe {
 
 template <typename T, std::size_t QUEUE_SIZE = 256,
           typename = typename std::enable_if<std::is_nothrow_copy_constructible<T>::value>::type,
           typename = typename std::enable_if<std::is_default_constructible<T>::value>::type>
-class DataQueue{
+class queue{
 private:
     struct node
     {
@@ -21,14 +74,14 @@ private:
     };
 
 public:
-    DataQueue():
+    queue():
         m_head(std::make_unique<node>()), m_tail(m_head.get())
     {}
 
-    DataQueue(const DataQueue& other) = delete;
-    DataQueue& operator= (const DataQueue& other) = delete;    
+    queue(const queue& other) = delete;
+    queue& operator= (const queue& other) = delete;
 
-    ~DataQueue() {
+    ~queue() {
         while(tryPop());
     }
 
@@ -113,6 +166,71 @@ public:
         }
     }
 
+    std::string storeToDisk(const char* name) {
+
+        std::string filename(name);
+        filename += std::to_string(getSecondsSinceEpoch());
+        filename += ".txt";
+
+        std::ofstream ifs(filename, std::ios_base::out | std::ios::binary);
+        if(ifs.is_open())
+        {
+            std::lock(m_headMutex, m_tailMutex);
+            std::unique_lock<std::mutex> headLock(m_headMutex, std::adopt_lock);
+            std::unique_lock<std::mutex> tailLock(m_tailMutex, std::adopt_lock);
+
+            if(m_head.get() != m_tail)
+            {
+                node* temp = m_head.get();
+                while (temp != m_tail) {
+                    T& data = *temp->data;
+                    write(data);
+                    temp = temp->next.get();
+                }
+            }
+        }
+
+        return filename;
+    }
+
+    bool tryReadFromDisk(const char* filename) {
+        std::ifstream ofs(filename, std::ios_base::in);
+        if(ofs.is_open())
+        {
+            std::string rawData;
+            ofs >> rawData;
+
+            unsigned long pos = 0;
+            while(!rawData.empty())
+            {                
+
+                if(!tryPush(read<T>(temp)))
+                    return false;
+                rawData.erase(rawData.begin(), std::next(rawData.begin(), static_cast<long>(pos + 1)));
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    void waitReadFromDisk(const char* filename) {
+        std::ifstream ofs(filename, std::ios_base::in | std::ios_base::trunc);
+        if(ofs.is_open())
+        {
+            std::string rawData;
+            ofs >> rawData;
+
+            unsigned long pos = 0;
+            while(pos != std::string::npos)
+            {
+                pos = rawData.find_first_of(DELIMETER);
+                std::string temp(rawData.begin(), std::next(rawData.begin(), static_cast<long>(pos)));
+                waitPush(read<T>(temp));
+                rawData.erase(rawData.begin(), std::next(rawData.begin(), static_cast<long>(pos)));
+            }
+        }
+    }
 private:
     node* getTail()
     {
@@ -208,3 +326,4 @@ private:
     std::condition_variable m_dataAwaiting;
     std::condition_variable m_roomAwaiting;
 };
+} // namespace threadsafe
